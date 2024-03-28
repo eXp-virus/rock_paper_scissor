67b5654783eed6350cf04ba64c7b26fedfca92bb
Rupesh <56559923+eXp-virus@users.noreply.github.com>
2024-03-28 22:27:31 +0530

    Add more features
    Made it a 5 round game, added scores, declares winner.

--- script.js
+++ script.js
@@ -26,7 +26,39 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-let playerSelection = prompt("Your choice: ").toLowerCase();
-let computerSelection = getComputerChoice();
-console.log("Computer:", computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+let playerSelection;
+let computerSelection;
+
+// variables to track scores
+let playerScore = 0;
+let computerScore = 0;
+
+//play a 5 round game
+function PlayGame() {
+    for(let i=0; i<5; i++) {
+        playerSelection = prompt("Your choice: ").toLowerCase();
+        computerSelection = getComputerChoice();
+        console.log("Computer:", computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+        //create empty space after every round to make console output look pretty
+        console.log("")
+        //keep track of score
+        if(playRound(playerSelection, computerSelection) === "You win!") {
+            playerScore++;
+        } else if(playRound(playerSelection, computerSelection) === "You lose!") {
+            computerScore++;
+        }
+    }
+ }
+
+PlayGame();
+console.log("Your score:", playerScore);
+console.log("Computer's score:", computerScore);
+//display winner
+if(playerScore > computerScore) {
+    console.log("Congrats! You beat the computer")
+} else if(computerScore > playerScore) {
+    console.log("You lost to computer. Better luck next time!!")
+} else {
+    console.log("Game was a tie!")
+}
