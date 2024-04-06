23ff5b6a76f2588d46cd6c12be34933b2f208eef
Rupesh <56559923+eXp-virus@users.noreply.github.com>
2024-04-06 14:43:04 +0530

    Display winner

--- script.js
+++ script.js
@@ -36,6 +36,8 @@ let computerScore = 0;
 const btn = document.querySelector("#selection");
 let pScore = document.querySelector('#pscore');
 let cScore = document.querySelector('#cscore');
+let compChoice = document.querySelector('#compChoice');
+
 btn.addEventListener('click', (event) => {
     let target = event.target;
     computerSelection = getComputerChoice();
@@ -43,33 +45,46 @@ btn.addEventListener('click', (event) => {
     let result = document.querySelector('#result');
     switch (target.id) {
         case 'rock':
-            console.log("Computer's choice:", computerSelection)
+            
             roundWinner = playRound('rock', computerSelection);
             calScore();
+            compChoice.textContent = `Computer: ${computerSelection}`;
             result.textContent = `${roundWinner}`;
             break;
 
         case 'paper':
-            console.log("Computer's choice:", computerSelection)
             roundWinner = playRound('paper', computerSelection);
             calScore();
+            compChoice.textContent = `Computer: ${computerSelection}`;
             result.textContent = `${roundWinner}`;
             break;
-        case 'scissor':
 
-            console.log("Computer's choice:", computerSelection)
+        case 'scissor':
             roundWinner = playRound('scissor', computerSelection);
             calScore();
+            compChoice.textContent = `Computer: ${computerSelection}`;
             result.textContent = `${roundWinner}`;
             break;
 
     }
     pScore.textContent = `Your Score: ${playerScore}`;
     cScore.textContent = `Computer's Score: ${computerScore}`;
-
+    displayWinner();
 });
 
 function calScore() {
     if(roundWinner == 'You win!') playerScore++;
     else if(roundWinner == 'You lose!') computerScore++;
+}
+
+buttons = document.querySelectorAll('button');
+function displayWinner() {
+    if(playerScore == 5 || computerScore == 5) {
+       buttons.forEach(elem => { // disable buttons
+        elem.disabled = true;
+
+        if(playerScore > computerScore) result.textContent = `You won the game!`;
+        else result.textContent = `You lost the game!`;
+       }) 
+    }
 }
