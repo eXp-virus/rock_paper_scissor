b4cc6e70ff0317330602ed67b069815e19ec01cb
Rupesh <56559923+eXp-virus@users.noreply.github.com>
2024-04-06 12:23:23 +0530

    Add buttons to plsy and display result

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rock_paper_scissor
+Play a simple game of rock paper scissor with computer.
--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
-    if(choice === 0) {return "rock";}
-    else if(choice === 1) {return "paper";}
-    else {return "scissor";}
+    if (choice === 0) { return "rock"; }
+    else if (choice === 1) { return "paper"; }
+    else { return "scissor"; }
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "It's a tie!";
     } else {
-        
-        if(playerSelection === 'rock' && computerSelection === 'paper') {
+
+        if (playerSelection === 'rock' && computerSelection === 'paper') {
             return "You lose!";
-        } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
             return "You win!";
-        } else if(playerSelection === 'paper' && computerSelection === 'scissor') {
+        } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
             return "You lose!";
-        } else if(playerSelection === 'scissor' && computerSelection === 'paper') {
+        } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
             return "You win!";
-        } else if(playerSelection === 'scissor' && computerSelection === 'rock') {
+        } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
             return "You lose!";
-        } else if(playerSelection === 'rock' && computerSelection === 'scissor') {
+        } else if (playerSelection === 'rock' && computerSelection === 'scissor') {
             return "You win!";
         }
     }
@@ -29,41 +29,33 @@ function playRound(playerSelection, computerSelection) {
 let playerSelection;
 let computerSelection;
 
-// variables to track scores
+let roundWinner;
 let playerScore = 0;
 let computerScore = 0;
 
-//play a 5 round game
-function PlayGame() {
-    for(let i=0; i<5; i++) {
-        playerSelection = prompt("Your choice: ").toLowerCase();
-        //continue prompting until user enters a valid input
-        while(playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissor") {
-            console.log("Please enter valid input")
-            playerSelection = prompt("Your choice: ").toLowerCase();
-        }
-        computerSelection = getComputerChoice();
-        console.log("Computer:", computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-        //create empty space after every round to make console output look pretty
-        console.log("")
-        //keep track of score
-        if(playRound(playerSelection, computerSelection) === "You win!") {
-            playerScore++;
-        } else if(playRound(playerSelection, computerSelection) === "You lose!") {
-            computerScore++;
-        }
-    }
- }
+const btn = document.querySelector("#selection");
+btn.addEventListener('click', (event) => {
+    let target = event.target;
+    computerSelection = getComputerChoice();
 
-PlayGame();
-console.log("Your score:", playerScore);
-console.log("Computer's score:", computerScore);
-//display winner
-if(playerScore > computerScore) {
-    console.log("Congrats! You beat the computer")
-} else if(computerScore > playerScore) {
-    console.log("You lost to computer. Better luck next time!!")
-} else {
-    console.log("Game was a tie!")
-}
+    let result = document.querySelector('#result');
+    switch (target.id) {
+        case 'rock':
+            console.log("Computer's choice:", computerSelection)
+            //console.log(playRound('rock', computerSelection))
+            result.textContent = `${playRound('rock', computerSelection)}`;
+            break;
+
+        case 'paper':
+            console.log("Computer's choice:", computerSelection)
+            result.textContent = `${playRound('paper', computerSelection)}`;
+            break;
+        case 'scissor':
+
+            console.log("Computer's choice:", computerSelection)
+            result.textContent = `${playRound('scissor', computerSelection)}`;
+            break;
+
+    }
+
+});
